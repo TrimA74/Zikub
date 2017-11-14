69c5676c375812fbd09f81174d5d2ea71b1a04eb
TrimA <florianseb96@hotmail.fr>
2017-11-14 20:16:18 +0100

    changed design of the app, need to fix somes colors for some views add menu with logout button

--- app/src/main/java/sthioul/olivier/zikub/MainActivity.java
+++ app/src/main/java/sthioul/olivier/zikub/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -298,6 +299,13 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
         }
 
         //Toast.makeText(MainActivity.this, "resume" ,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
 
